92148abdcfb8a092c5c7ddc76cf1e34cbbcf6bf5
srhhttnr <shuettner12@gmail.com>
2024-06-03 10:40:14 -0500

    replaced hardcoded movies array as the initial value with an empty array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,29 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: 'Barbie',
-      Director: 'Greta Gerwig',
-      Genre: 'Comedic Fantasy',
-      ImageURL: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Barbie_2023_poster.jpg'
-    },
-    {
-      id: 2,
-      Title: 'Pride and Prejudice',
-      Director: 'Joe Wright',
-      Genre: 'Romantic Drama',
-      ImageURL: 'https://upload.wikimedia.org/wikipedia/en/0/03/Prideandprejudiceposter.jpg'
-    },
-    {
-      id: 3,
-      Title: 'Step Brothers',
-      Director: 'Adam McKay',
-      Genre: 'Comedy',
-      ImageURL: 'https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg' 
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
 
   const [selectedMovie, setSelectedMovie] = useState(null);
