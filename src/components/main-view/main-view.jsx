eff007976cf58c7c94a4bf30ddc00c311c396ee1
srhhttnr <shuettner12@gmail.com>
2024-06-03 12:59:35 -0500

    fetched movies array from myFlix API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
 
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((movies) => {
+        const moviesFromApi = movies.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            director: movie.Director,
+            genre: movie.Genre,
+            releaseDate: movie.Release,
+            imagePath: movie.Image,
+            featured: movie.Featured
+          }
+        });
+        setMovies(moviesFromApi);
+      })
+  }, []);
 
   if (selectedMovie) {
     return (
