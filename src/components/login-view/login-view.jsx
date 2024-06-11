d42a38cdb0af5ac3057093b83221ca3f222a0d27
srhhttnr <shuettner12@gmail.com>
2024-06-11 12:16:47 -0500

    added local storage to persist login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -7,25 +7,37 @@ export const LoginView = ( onLoggedIn ) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     const data = {
-      access: username,
-      secret: password
+      Username: username,
+      Password: password
     };
 
     fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/login", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data)
-    }).then((response) => {
-      if (response.ok) {
-        onLoggedIn(username);
-      } else {
-        alert("Login failed");
-      }
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
   };
   
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
         <input
