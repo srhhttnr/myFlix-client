5677bf817eefe71fbb7b8af7b5bcdb682cfc02d8
srhhttnr <shuettner12@gmail.com>
2024-06-11 11:27:31 -0500

    created login form

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+
+export const LoginView = ( onLoggedIn ) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
+  };
+  
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
