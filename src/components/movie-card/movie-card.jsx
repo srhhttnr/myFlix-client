00e8b422c22923775e089d322a6cf99918819bb5
srhhttnr <shuettner12@gmail.com>
2024-07-29 15:39:48 -0500

    implemented react router

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movie.imagePath} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.director.Name}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Open
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`} >
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -26,5 +28,4 @@ MovieCard.propTypes = {
     director: PropTypes.object.isRequired,
     featured: PropTypes.bool
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
