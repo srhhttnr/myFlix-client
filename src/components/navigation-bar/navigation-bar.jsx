e3e7d54557cf57689be07498e2bbc09cb8c76c3c
srhhttnr <shuettner12@gmail.com>
2024-09-03 20:37:39 -0500

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="navbar navbar-light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
           MyFlix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto navbar-item">
             {!user && (
               <>
                 <Nav.Link as={Link} to="/login">
