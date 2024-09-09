eb6db946cb2a9ab1467c59e7ee2e0d912f00f517
srhhttnr <shuettner12@gmail.com>
2024-09-08 20:47:44 -0500

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Button, Form, Container } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = ({ movies }) => {
+export const ProfileView = () => {
   const localUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(localUser ? localUser : null);  
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const favMovies = movies.filter((movie) => {
     return localUser.FavoriteMovies.includes(movie.id);
   });
@@ -13,69 +16,23 @@ export const ProfileView = ({ movies }) => {
   const [email, setEmail] = useState(localUser.Email || "");
   const [birthday, setBirthday] = useState(localUser.Birthday || "01/01/0001");
 
-  // const deleteUser = (event) => {
-  //   event.preventDefault();
-
-  //   const data = {
-  //     Username: username,
-  //     Password: password,
-  //     Confirmation: yes
-  //   };
-
-  //   fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/users", {
-  //     method: "DELETE",
-  //     body: JSON.stringify(data),
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     }
-  //     }).then((response) => {
-  //       if (response.ok) {
-  //         alert("Account deleted");
-  //         window.location.reload();
-  //       } else {
-  //         alert("Account not deleted");
-  //       }
-  //   });
-
-  //   return (
-  //     <Form onSubmit={deleteUser}>
-  //       <Form.Group controlId="formUsername">
-  //         <Form.Label>Username:</Form.Label>
-  //         <Form.Control
-  //           type="text"
-  //           value={username}
-  //           onChange={(e) => setUsername(e.target.value)}
-  //           required
-  //           minLength="3"
-  //         />
-  //       </Form.Group>
-
-  //       <Form.Group controlId="formPassword">
-  //         <Form.Label>Password:</Form.Label>
-  //         <Form.Control
-  //           type="text"
-  //           value={password}
-  //           onChange={(e) => setPassword(e.target.value)}
-  //           required
-  //           minLength="3"
-  //         />
-  //       </Form.Group>
-
-  //       <Form.Group controlId="formConfirmation">
-  //         <Form.Label>Confirm Account Delete:</Form.Label>
-  //         <Form.Control
-  //           type="text"
-  //           value={yes}
-  //         />
-  //       </Form.Group>
-
-  //       <Button variant="primary" type="submit">
-  //         Delete Account
-  //       </Button>
-
-  //     </Form>
-  //   )
-  // }
+  useEffect(() => {
+    if (!user) {
+      fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/users", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setUser(data); // Set user state with fetched data
+        })
+        .catch((error) => console.error("Error fetching user data:", error));
+    }
+  }, [user, token, localUser.Username]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -93,64 +50,99 @@ export const ProfileView = ({ movies }) => {
       headers: {
         "Content-Type": "application/json"
       }
-      }).then((response) => {
-        if (response.ok) {
+      }).then((response) => response.json())
+        .then((updatedUser) => {
+          setUser(updatedUser);
+          localStorage.setItem("user", JSON.stringify(updatedUser));
           alert("User update successful");
           window.location.reload();
-        } else {
-          alert("User update failed");
-        }
-    });
+        })
+      .catch((error) => {
+        console.error("Error updating user information:", error);
+      });
   };
+
+  const handleDeleteUser = (event) => {
+    event.preventDefault();
+
+    fetch("https://my-movies-db-cafa6b5db6b8.herokuapp.com/users", {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,        
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert("User account deleted successfully");
+        localStorage.clear();
+        window.location.reload();
+      } else {
+        throw new Error("Failed to delete user account");
+      }
+    })
+    .catch((error) => {
+      console.error("Error deleting user account:", error);
+    });
+  }
 
   console.log(favMovies);
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="3"
-        />
-      </Form.Group>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3"
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minLength="3"
-        />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="3"
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
 
-      <Button variant="dark" type="submit">
-        Update User Information
+        <Button variant="dark" type="submit">
+          Update User Information
+        </Button>
+
+      </Form>
+
+      <Button 
+        variant="dark"
+        style={{ cursor: "pointer" }}
+        onClick={handleDeleteUser}
+      >
+        Delete Account
       </Button>
 
       <div>
@@ -165,6 +157,6 @@ export const ProfileView = ({ movies }) => {
           />)
         })
       }
-    </Form>
+    </Container>
   )
 }
